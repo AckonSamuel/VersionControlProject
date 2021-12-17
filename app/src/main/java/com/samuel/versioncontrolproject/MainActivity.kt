4efe748bd186f2d03de4671aeaa2e92c23babca5
Samuel Ackon <ackons433@gmail.com>
2021-12-17 15:42:34 +0000

    adding google sign method to project

--- app/src/main/java/com/samuel/versioncontrolproject/MainActivity.kt
+++ app/src/main/java/com/samuel/versioncontrolproject/MainActivity.kt
@@ -1,17 +1,124 @@
 package com.samuel.versioncontrolproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.samuel.versioncontrolproject.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var googleSignInClient: GoogleSignInClient
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    private companion object{
+        //private const val RC_SIGN_IN = 100
+        private const val TAG = "GOOGLE_SIGN_IN_TAG"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding= ActivityMainBinding.inflate(LayoutInflater.from(this))
         val view = binding.root
         setContentView(view)
+
+        val googleSignInOptions = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        googleSignInClient = GoogleSignIn.getClient(this,googleSignInOptions)
+
+        firebaseAuth= FirebaseAuth.getInstance()
+        checkUser()
+
+        val getAction = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+            val accountTask = GoogleSignIn.getSignedInAccountFromIntent(it.data)
+            val account = accountTask.getResult(ApiException::class.java)
+            firebaseAuthWithGoogleAccout(account)
+        }
+        binding.googleSignInBtn.setOnClickListener{
+            Log.d(TAG, "onCreate: begin Google SignIn")
+            val intent = googleSignInClient.signInIntent
+           // startActivityForResult(intent, RC_SIGN_IN)
+            getAction.launch(intent)
+        }
+    }
+
+    private fun checkUser() {
+        val firebaseUser = firebaseAuth.currentUser
+        if(firebaseUser!=null){
+            startActivity(Intent(this@MainActivity, ProfileActivity::class.java))
+            finish()
+        }
+
+    }
+
+    /*override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == RC_SIGN_IN){
+            Log.d(TAG, "onActivityResult: Google SignIn intent result")
+            val accountTask = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try{
+                val account = accountTask.getResult(ApiException::class.java)
+                firebaseAuthWithGoogleAccout(account)
+        }
+            catch (e: Exception){
+                Log.d(TAG, "onActivityResult: ${e.message}")
+
+            }            }
+    }*/
+
+    private fun firebaseAuthWithGoogleAccout(account: GoogleSignInAccount?) {
+        Log.d(TAG, "firebaseAuthWithGoogleAccount: begin firebase auth with google account")
+        val credential = GoogleAuthProvider.getCredential(account!!.idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnSuccessListener { authResult ->
+                Log.d(TAG, "firebaseAuthWithGoogleAccount: LoggedIn")
+
+                val firebaseUser = firebaseAuth.currentUser
+                val uid = firebaseUser!!.uid
+                val email = firebaseUser.email
+
+                Log.d(TAG, "firebaseAuthWithGoogleAccount: Uid: $uid ")
+                Log.d(TAG, "firebaseAuthWithGoogleAccount: Email: $email ")
+
+                if (authResult.additionalUserInfo!!.isNewUser) {
+                    Log.d(TAG, "firebaseAuthWithGoogleAccount: Account created... \n$email")
+                    Toast.makeText(
+                        this@MainActivity,
+                        "Account created... \n$email",
+                        Toast.LENGTH_LONG
+                    ).show()
+                } else {
+                    Log.d(TAG, "firebaseAuthWithGoogleAccount: Existing user... \n$email")
+                    Toast.makeText(this@MainActivity, "LoggedIn... \n$email", Toast.LENGTH_LONG)
+                        .show()
+
+                }
+                startActivity(Intent(this@MainActivity, ProfileActivity::class.java))
+                finish()
+
+            }
+            .addOnFailureListener{ e ->
+                Log.d(TAG, "firebaseAuthWithGoogleAccount: Log in Failed due to ${e.message}")
+                Toast.makeText(this@MainActivity, "Log in Failed due to ${e.message}", Toast.LENGTH_LONG).show()
+            }
+
     }
 }
---
+++ app/src/main/java/com/samuel/versioncontrolproject/ProfileActivity.kt
@@ -0,0 +1,49 @@
+package com.samuel.versioncontrolproject
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import com.google.firebase.auth.FirebaseAuth
+import com.samuel.versioncontrolproject.databinding.ActivityProfileBinding
+
+class ProfileActivity : AppCompatActivity() {
+    //view binding
+    private lateinit var binding: ActivityProfileBinding
+
+    //firebase auth
+    private lateinit var firebaseAuth: FirebaseAuth
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityProfileBinding.inflate(LayoutInflater.from(this))
+        val view = binding.root
+        setContentView(view)
+
+        //init firebase auth
+        firebaseAuth= FirebaseAuth.getInstance()
+        checkUser()
+
+        //handle click, logout user
+        binding.logoutBtn.setOnClickListener {
+            firebaseAuth.signOut()
+            checkUser()
+        }
+
+    }
+
+    private fun checkUser() {
+        //get current user
+        val firebaseUser = firebaseAuth.currentUser
+        if(firebaseUser == null){
+            //user not logged in
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+        else{
+            //user logged in
+            //get user info
+            val email=firebaseUser.email
+            //set email
+            binding.emailTv.text = email
+        }
+    }
+}
